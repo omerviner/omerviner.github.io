9fa0ec635ad7b0975e7e54835d51dd037b097905
Omer Viner <omer.viner@mail.huji.ac.il>
2017-02-19 02:10:00 +0200

    pages & design

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-js
+$(document).ready(function(){
+	$("#projectsLink").addClass("chosen");
+
+	$('#bottomText').prepend("© " + (new Date).getFullYear());
+	
+    $("a").click(function(){
+		$("#projectsLink").removeClass("chosen");
+		$("a").removeClass("chosen");
+        $(this).addClass("chosen");
+		if ($(this).attr("id") == "myNameLink"){
+			$(this).removeClass("chosen");
+			$("#projectsLink").addClass("chosen");
+
+		}
+    });
+});
