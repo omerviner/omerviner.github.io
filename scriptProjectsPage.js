b4d18b59cae9f320ab64b7bffe8312fbad9de8d5
Omer Viner <omer.viner@mail.huji.ac.il>
2017-03-05 23:57:18 +0200

    *

--- scriptProjectsPage.js
+++ scriptProjectsPage.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$(".imgContainer").click(function(){
-		console.log("omer");
 		var name = $(this).attr("id");
+		parent.postMessage(name, "https://omerviner.github.io/portfolio/");
 		});
 
 });
