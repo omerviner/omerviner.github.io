af09fba8c6305e1843aef613373f210b18d30a41
Omer Viner <omer.viner@mail.huji.ac.il>
2017-03-05 23:39:17 +0200

    *

--- script.js
+++ script.js
@@ -14,7 +14,23 @@ $(document).ready(function(){
 		}
     });
 	
-	$(".imgContainer").click(function(){
-		alert("a");
-	}); 	
+	window.addEventListener('message', function(event) { 
+
+		// IMPORTANT: Check the origin of the data! 
+		if (~event.origin.indexOf('http://yoursite.com')) { 
+			// The data has been sent from your site 
+
+			// The data sent with postMessage is stored in event.data 
+			console.log(event.data); 
+		} else { 
+			// The data hasn't been sent from your site! 
+			// Be careful! Do not use it. 
+			return; 
+		} 
+	}); 
+
+
+
+
+
 });
